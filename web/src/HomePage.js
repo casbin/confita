bbd5a1732720c57fda9fe29caec1f137627df38f
Gucheng Wang <nomeguy@qq.com>
2021-08-17 09:30:43 +0800

    Finish home page.

--- web/src/HomePage.js
+++ web/src/HomePage.js
@@ -1,17 +1,41 @@
 import React from "react";
+import Conference from "./Conference";
+import * as ConferenceBackend from "./backend/ConferenceBackend";
+
+const owner = "admin";
+const conferenceName = "conference_0";
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       classes: props,
+      conferenceName: conferenceName,
+      conference: null,
     };
   }
 
+  componentWillMount() {
+    this.getConference();
+  }
+
+  getConference() {
+    ConferenceBackend.getConference(owner, this.state.conferenceName)
+      .then((conference) => {
+        this.setState({
+          conference: conference,
+        });
+      });
+  }
+
   render() {
+    if (this.state.conference === null) {
+      return null;
+    }
+
     return (
       <div>
-        hello
+        <Conference conference={this.state.conference} />
       </div>
     )
   }
