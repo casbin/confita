1046dacdf8f6cff8fc32e91a46881793974810ab
Gucheng Wang <nomeguy@qq.com>
2022-02-03 01:55:43 +0800

    Fix tree drag.

--- web/src/ConferenceEdit.js
+++ web/src/ConferenceEdit.js
@@ -18,41 +18,12 @@ import {DeleteOutlined, PlusOutlined,} from "@ant-design/icons";
 import * as Setting from "./Setting";
 import HtmlEditorBraft from "./HtmlEditorBraft";
 
-const x = 3;
-const y = 2;
-const z = 1;
-const gData = [];
-
-const generateData = (_level, _preKey, _tns) => {
-  const preKey = _preKey || '0';
-  const tns = _tns || gData;
-
-  const children = [];
-  for (let i = 0; i < x; i++) {
-    const key = `${preKey}-${i}`;
-    tns.push({ title: key, key });
-    if (i < y) {
-      children.push(key);
-    }
-  }
-  if (_level < 0) {
-    return tns;
-  }
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
-
 class ConferenceEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       classes: props,
       treePath: '0-0',
-      gData,
       expandedKeys: ['0-0', '0-0-0', '0-0-0-0'],
     };
   }
@@ -69,67 +40,22 @@ class ConferenceEdit extends React.Component {
   };
 
   onDrop = info => {
-    console.log(info);
-    const dropKey = info.node.key;
-    const dragKey = info.dragNode.key;
-    const dropPos = info.node.pos.split('-');
-    const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+    // console.log(info);
 
-    const loop = (data, key, callback) => {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].key === key) {
-          return callback(data[i], i, data);
-        }
-        if (data[i].children) {
-          loop(data[i].children, key, callback);
-        }
-      }
-    };
-    const data = [...this.state.gData];
-
-    // Find dragObject
-    let dragObj;
-    loop(data, dragKey, (item, index, arr) => {
-      arr.splice(index, 1);
-      dragObj = item;
-    });
-
-    if (!info.dropToGap) {
-      // Drop on the content
-      loop(data, dropKey, item => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-      });
-    } else if (
-      (info.node.props.children || []).length > 0 && // Has children
-      info.node.props.expanded && // Is expanded
-      dropPosition === 1 // On the bottom gap
-    ) {
-      loop(data, dropKey, item => {
-        item.children = item.children || [];
-        // where to insert 示例添加到头部，可以是随意位置
-        item.children.unshift(dragObj);
-        // in previous version, we use item.children.push(dragObj) to insert the
-        // item to the tail of the children
-      });
-    } else {
-      let ar;
-      let i;
-      loop(data, dropKey, (item, index, arr) => {
-        ar = arr;
-        i = index;
-      });
-      if (dropPosition === -1) {
-        ar.splice(i, 0, dragObj);
-      } else {
-        ar.splice(i + 1, 0, dragObj);
-      }
+    const dragIndex = parseInt(info.dragNode.pos.slice(2));
+    let dropIndex = parseInt(info.node.pos.slice(2));
+    if (dropIndex !== 0 && dropIndex < dragIndex) {
+      dropIndex += 1;
     }
+    // console.log(dragIndex);
+    // console.log(dropIndex);
 
-    this.setState({
-      gData: data,
-    });
+    let treeItems = this.props.conference.treeItems;
+
+    const treeItem = treeItems[dragIndex];
+    treeItems = Setting.deleteRow(treeItems, dragIndex);
+    treeItems = Setting.insertRow(treeItems, treeItem, dropIndex);
+    this.props.onUpdateTreeItems(treeItems);
   };
 
   updateTree(tree) {
@@ -227,9 +153,6 @@ class ConferenceEdit extends React.Component {
       // alert(JSON.stringify(treeItem));
     };
 
-    // let data = `[{"title":"0-0","key":"0-0","children":[{"title":"0-0-0","key":"0-0-0","children":[{"title":"0-0-0-0","key":"0-0-0-0"},{"title":"0-0-0-1","key":"0-0-0-1"},{"title":"0-0-0-2","key":"0-0-0-2"}]},{"title":"0-0-1","key":"0-0-1","children":[{"title":"0-0-1-0","key":"0-0-1-0"},{"title":"0-0-1-1","key":"0-0-1-1"},{"title":"0-0-1-2","key":"0-0-1-2"}]},{"title":"0-0-2","key":"0-0-2"}]},{"title":"0-1","key":"0-1","children":[{"title":"0-1-0","key":"0-1-0","children":[{"title":"0-1-0-0","key":"0-1-0-0"},{"title":"0-1-0-1","key":"0-1-0-1"},{"title":"0-1-0-2","key":"0-1-0-2"}]},{"title":"0-1-1","key":"0-1-1","children":[{"title":"0-1-1-0","key":"0-1-1-0"},{"title":"0-1-1-1","key":"0-1-1-1"},{"title":"0-1-1-2","key":"0-1-1-2"}]},{"title":"0-1-2","key":"0-1-2"}]},{"title":"0-2","key":"0-2"}]`
-    // alert(JSON.stringify(this.state.gData))
-
     const copiedTreeItems = treeItems.map((treeItem, i) => {
       let copiedTreeItem = Setting.deepCopy(treeItem);
       copiedTreeItem.title = (
@@ -264,7 +187,6 @@ class ConferenceEdit extends React.Component {
           showIcon={true}
           defaultSelectedKeys={treeItems.length > 0 ? [treeItems[0].key] : []}
           onSelect={onSelect}
-          // treeData={this.state.gData}
           treeData={copiedTreeItems}
         />
       </div>
