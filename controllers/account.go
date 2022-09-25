75a877a5dec5f311b8f246d5dc2718e1f450ee15
Gucheng Wang <nomeguy@qq.com>
2022-09-25 09:38:29 +0800

    Improve error in Signin()

--- controllers/account.go
+++ controllers/account.go
@@ -44,12 +44,14 @@ func (c *ApiController) Signin() {
 
 	token, err := auth.GetOAuthToken(code, state)
 	if err != nil {
-		panic(err)
+		c.ResponseError(err.Error())
+		return
 	}
 
 	claims, err := auth.ParseJwtToken(token.AccessToken)
 	if err != nil {
-		panic(err)
+		c.ResponseError(err.Error())
+		return
 	}
 
 	claims.AccessToken = token.AccessToken
