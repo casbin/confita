041c888754a7208c1f7a59a71efbf9dc6bcf0e01
Chell <im.chell@outlook.com>
2022-10-20 01:46:59 +0200

    fix: instant translation

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -17,6 +17,7 @@ import zh from "./locales/zh/data.json";
 import en from "./locales/en/data.json";
 import * as Conf from "./Conf";
 import * as Setting from "./Setting";
+import {initReactI18next} from "react-i18next";
 
 const resources = {
   en: en,
@@ -53,7 +54,7 @@ function initLanguage() {
   return language;
 }
 
-i18n.init({
+i18n.use(initReactI18next).init({
   lng: initLanguage(),
 
   resources: resources,
