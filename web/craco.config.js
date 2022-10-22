ea787fdadb24a175b4bbee3e65da27fb220ffa7c
8208201214 <8208201214@csu.edu.cn>
2022-10-22 17:40:33 +0800

    feat: add pre commit and fix pass bugs

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,4 +1,4 @@
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require("craco-less");
 
 module.exports = {
   plugins: [
@@ -7,7 +7,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { "@primary-color": "rgb(26,115,232)", "@border-radius-base": "5px" },
+            modifyVars: {"@primary-color": "rgb(26,115,232)", "@border-radius-base": "5px"},
             javascriptEnabled: true,
           },
         },
